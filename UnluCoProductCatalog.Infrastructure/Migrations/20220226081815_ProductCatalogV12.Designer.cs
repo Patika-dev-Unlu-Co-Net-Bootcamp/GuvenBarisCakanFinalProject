b01028d07e72dae690fa39e928ad18aa58dda250
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-02-26 13:36:53 +0300

    Migration regularized. Product Category relation added.

--- UnluCoProductCatalog.Infrastructure/Migrations/20220226081815_ProductCatalogV12.Designer.cs
+++ UnluCoProductCatalog.Infrastructure/Migrations/20220226081815_ProductCatalogV12.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UnluCoProductCatalog.Infrastructure.Contexts;
 
 namespace UnluCoProductCatalog.Infrastructure.Migrations
 {
     [DbContext(typeof(ProductCatalogDbContext))]
-    partial class ProductCatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220226081815_ProductCatalogV12")]
+    partial class ProductCatalogV12
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
